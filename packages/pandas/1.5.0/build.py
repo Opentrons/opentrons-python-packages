8a79af9b67e72f10bea6415231fc2d937cbd1433
Seth Foster <seth@opentrons.com>
2022-10-03 16:14:08 -0400

    add command to pandas build

--- packages/pandas/1.5.0/build.py
+++ packages/pandas/1.5.0/build.py
@@ -4,5 +4,6 @@ build.build_package(
     source=build.github_source(
         org='pandas-dev',
         repo='pandas',
-        tag='v1.5.0')
+        tag='v1.5.0'),
+    setup_py_command='build_ext'
 )

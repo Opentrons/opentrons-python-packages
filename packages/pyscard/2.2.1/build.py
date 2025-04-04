3c62b07f0b334ab1ccfbf198de7769b2df0ef630
Josh McVey <yersh13@gmail.com>
2025-04-04 11:41:34 -0500

    this built

--- packages/pyscard/2.2.1/build.py
+++ packages/pyscard/2.2.1/build.py
@@ -2,9 +2,9 @@ from builder import package_build
 
 package_build.build_package(
     source=package_build.github_source(
-        org='pandas-dev',
-        repo='pandas',
-        tag='v1.5.0'),
+        org='LudovicRousseau',
+        repo='pyscard',
+        tag='2.2.1'),
     setup_py_commands=['build_ext', 'bdist_wheel'],
-    build_dependencies=['numpy', 'Cython']
+    build_dependencies=[]
 )
